54bc59e73554ccfff26809c48d945e05752c3a14
Sohom Ghosh <sohom1ghosh@gmail.com>
2018-01-18 16:32:58 +0530

    Update flask_api_make.py

--- flask_api_make.py
+++ flask_api_make.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
+@cross_origin(supports_credentials=True)
 @app.route('/')
 def my_form():
     return render_template('my-form.html')
@@ -12,3 +15,6 @@ def my_form_post():
     processed_text = text.upper()
     return processed_text
     
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="172.189.189.190",debug=True)
